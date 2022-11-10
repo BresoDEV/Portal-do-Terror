5eba575201d9c9127ecd3d7f066004a057f1690d
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2022-11-10 08:23:57 -0300

    Add files via upload

---
+++ codigos/olho.php
@@ -0,0 +1,7 @@
+<?php
+		//  header("Location: ../login.php"); exit;
+		echo "<body bgcolor='Black'>";
+	  echo "<center><img src='../ImagensSite/boo.gif' width=60% Heigth=80% ></center>";
+ ?>
+ 
+ 
---
+++ codigos/saw.php
@@ -0,0 +1,7 @@
+<?php
+		//  header("Location: ../login.php"); exit;
+		echo "<body bgcolor='Black'>";
+	  echo "<center><img src='../ImagensSite/saw.gif' width=100% Heigth=100px ></center>";
+ ?>
+ 
+ 
