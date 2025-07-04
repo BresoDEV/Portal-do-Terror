2ef923b4d1e49828876dc9988bd9d7f65fcffa45
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2025-07-04 07:17:03 -0300

    Commit automatico em 04/07/2025 ├ás  7:16:40,02

--- codigos/olho.php
+++
@@ -1,7 +0,0 @@
-<?php
-		//  header("Location: ../login.php"); exit;
-		echo "<body bgcolor='Black'>";
-	  echo "<center><img src='../ImagensSite/boo.gif' width=60% Heigth=80% ></center>";
- ?>
- 
- 
--- codigos/samara.php
+++
@@ -1,7 +0,0 @@
-<?php
-		//  header("Location: ../login.php"); exit;
-		echo "<body bgcolor='Black'>";
-	  echo "<center><img src='../ImagensSite/chamado.gif' width=100% Heigth=100px ></center>";
- ?>
- 
- 
