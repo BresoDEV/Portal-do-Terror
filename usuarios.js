5aee294409e70225afd1198bc8be809cb633954a
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2025-07-04 18:39:46 -0300

    Commit automatico em 04/07/2025 ├ás 18:39:45,88

---
+++ usuarios.js
@@ -0,0 +1,19 @@
+ // Lista de usuários com senha e deviceKey manual
+ const usuarios = {
+    "eduardo": {
+      senha: "123Aa321",
+      deviceKey: 'j7tn1hj11751618636492',
+      deviceKey2: "null",
+      deviceKey3: "ewgir26l1751662337535",
+      deviceKey4: "null",
+      plano: "completo"
+    },
+    "july": {
+      senha: "123Aa321",
+      deviceKey:  null,
+      deviceKey2: "null",
+      deviceKey3: "null",
+      deviceKey4: "null",
+      plano: "completo"
+    }
+  };
