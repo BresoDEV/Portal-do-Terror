c500a3189998c9510db5d351f762d487ad54e67e
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2025-07-06 21:43:59 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -3,10 +3,10 @@
     "eduardo": {
       id:"1",
       senha: "1",
-      deviceKey:  "g7s7o1zj1751823061634",//g7s7o1zj1751823061634
-      deviceKey2: "null",//celular
-      deviceKey3: "null",
-      deviceKey4: "null",
+      deviceKey:  "g7s7o1zj1751823061634",//pc gamer
+      deviceKey2: null,
+      deviceKey3: null,
+      deviceKey4: null,
       plano: "completo",
       telefone:"96180319",
       pagamento_em_dia: "sim",
